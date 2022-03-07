dcdbe989acf1e78f5a976ae720febcf44df26162
Josh Huckabee <joshhuckabee@gmail.com>
2022-03-07 09:18:31 -0500

    Scaffold application using Dart console template.

---
+++ bin/empora_address_validator.dart
@@ -0,0 +1,5 @@
+import 'package:empora_address_validator/empora_address_validator.dart' as empora_address_validator;
+
+void main(List<String> arguments) {
+  print('Hello world: ${empora_address_validator.calculate()}!');
+}
