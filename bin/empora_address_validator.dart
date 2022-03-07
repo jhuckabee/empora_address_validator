b96bd305ac3672ef0f2f4671cd37247ee934f615
Josh Huckabee <joshhuckabee@gmail.com>
2022-03-07 09:19:39 -0500

    Ignore editor/IDE artifacts.

--- bin/empora_address_validator.dart
+++
@@ -1,5 +0,0 @@
-import 'package:empora_address_validator/empora_address_validator.dart' as empora_address_validator;
-
-void main(List<String> arguments) {
-  print('Hello world: ${empora_address_validator.calculate()}!');
-}
--- test/empora_address_validator_test.dart
+++
@@ -1,8 +0,0 @@
-import 'package:empora_address_validator/empora_address_validator.dart';
-import 'package:test/test.dart';
-
-void main() {
-  test('calculate', () {
-    expect(calculate(), 42);
-  });
-}
